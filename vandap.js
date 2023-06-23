34a34165f430656e726f1e4118dbf6fa68d8b392
anhthang1507 <132344967+anhthang1507@users.noreply.github.com>
2023-06-23 22:30:21 +0900

    Add files via upload

---
+++ vandap.js
@@ -0,0 +1,13 @@
+const name = "bui anh thang";
+let name2 = "abc";
+let a = 2;
+let b = 3;
+function cong(a, b) {
+  return a + b;
+}
+console.log(cong(a, b));
+
+const cong2 = (a, b) => {
+  return a + b;
+};
+console.log(cong2(a, b));
